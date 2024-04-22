7e8476525fd71a11d37e11f70625d5bf8ac4653e
AlexRivasMachin <arivas028@ikasle.ehu.eus>
2024-04-22 15:38:11 +0200

    Initial commit

---
+++ app/src/main/java/com/lksnext/arivas/data/DataRepository.java
@@ -0,0 +1,29 @@
+package com.lksnext.arivas.data;
+
+import com.lksnext.arivas.domain.Callback;
+
+public class DataRepository {
+
+    private static DataRepository instance;
+    private DataRepository(){
+
+    }
+
+    //Creación de la instancia en caso de que no exista.
+    public static synchronized DataRepository getInstance(){
+        if (instance==null){
+            instance = new DataRepository();
+        }
+        return instance;
+    }
+
+    //Petición del login.
+    public void login(String email, String pass, Callback callback){
+        try {
+            //Realizar petición
+            callback.onSuccess();
+        } catch (Exception e){
+            callback.onFailure();
+        }
+    }
+}
